089495cc32ba9304bac675b02d243c7c3508b523
ToufeekS <toufeeks@outlook.com>
2026-09-02 17:11:21 +0100

    Implemented protocol filtering for sniffer

--- Network-Traffic-Analyser/sniffer.py
+++ Network-Traffic-Analyser/sniffer.py
@@ -1,17 +1,24 @@
 from scapy.all import sniff, IP, TCP, UDP
 
 print("Enter protocol to sniff (TCP/UDP or ALL): ")
-protocol = input().strip().upper
+protocol = input().strip().upper()
 
 def packet_callback(packet):
     if IP in packet:
         src = packet[IP].src
         dst = packet[IP].dst
         proto = packet[IP].proto    
-    
-    if TCP in packet:
-        print(f"TCP |  {src}:{packet[TCP].sport} -> {dst}:{packet[TCP].dport}")
-    elif UDP in packet:
-        print(f"UDP |  {src}:{packet[UDP].sport} -> {dst}:{packet[UDP].dport}")
+
+        if protocol == "ALL":    
+             if TCP in packet:
+                 print(f"TCP |  {src}:{packet[TCP].sport} -> {dst}:{packet[TCP].dport}")
+             elif UDP in packet:
+                print(f"UDP |  {src}:{packet[UDP].sport} -> {dst}:{packet[UDP].dport}")
+
+        elif protocol == "TCP" and TCP in packet:
+          print(f"TCP |  {src}:{packet[TCP].sport} -> {dst}:{packet[TCP].dport}")
+
+        elif protocol == "UDP" and UDP in packet:
+          print(f"UDP |  {src}:{packet[UDP].sport} -> {dst}:{packet[UDP].dport}")
 
 sniff(count=20, prn=packet_callback)
